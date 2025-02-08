ff26ec454fd7644a56bfdc4b26a141669d276593
matigoldberg14 <matigoldberg3@gmail.com>
2025-02-08 03:30:50 -0300

    landing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,20 @@ export default {
       backgroundImage: {
         'gradient-yuppie': 'linear-gradient(145deg, #5F50E5 0%, #2F02CC 100%)',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
 };
