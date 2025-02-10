7d5a28055de4745a86c42427c856e0045bad897e
matigoldberg14 <matigoldberg3@gmail.com>
2025-02-10 17:13:25 -0300

    vite 3 fireabse harcode public

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,21 +8,12 @@ let auth: Auth | undefined;
 if (typeof window !== 'undefined') {
   try {
     const firebaseConfig = {
-      apiKey:
-        import.meta.env.VITE_FIREBASE_API_KEY ??
-        'AIzaSyCUjUKgCdECVCEvIlK1OuB9mx61GtDdxEE',
-      authDomain:
-        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ??
-        'yuppiedashboard.firebaseapp.com',
-      projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? 'yuppiedashboard',
-      storageBucket:
-        import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ??
-        'yuppiedashboard.firebasestorage.app',
-      messagingSenderId:
-        import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ?? '619049806507',
-      appId:
-        import.meta.env.VITE_FIREBASE_APP_ID ??
-        '1:619049806507:web:0fe5772a82002b3b4225ee',
+      apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+      authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
     };
 
     const app = initializeApp(firebaseConfig);
