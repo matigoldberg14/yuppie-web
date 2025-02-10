b826fc846a3150928ddfeff0d6e19437f564c6d9
matigoldberg14 <matigoldberg3@gmail.com>
2025-02-10 17:28:31 -0300

    vuelta atrsasssss

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,12 @@
 interface ImportMetaEnv {
   readonly PUBLIC_API_URL: string;
   readonly PUBLIC_SITE_URL: string;
+  readonly PUBLIC_FIREBASE_API_KEY: string;
+  readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+  readonly PUBLIC_FIREBASE_PROJECT_ID: string;
+  readonly PUBLIC_FIREBASE_APP_ID: string;
+  readonly PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string;
 }
 
 interface ImportMeta {
