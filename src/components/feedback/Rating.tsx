32babeb8713e590e364020a778c23dc21045eadd
matigoldberg14 <matigoldberg3@gmail.com>
2025-04-29 20:13:35 -0300

    no omas cartelito en principio

--- src/components/feedback/Rating.tsx
+++ src/components/feedback/Rating.tsx
@@ -490,22 +490,6 @@ export function RatingForm({
             <span className="text-4xl transform transition-transform duration-200 group-hover:scale-110">
               {emoji}
             </span>
-
-            {/* Only show label when selected to improve performance */}
-            {selectedRating === rating && (
-              <div
-                className={`absolute -bottom-12 px-3 py-1 rounded-full text-sm text-white ${color} opacity-100`}
-              >
-                {label}
-              </div>
-            )}
-
-            {/* Indicator dot when selected */}
-            {selectedRating === rating && (
-              <div
-                className={`absolute -bottom-2 w-2 h-2 rounded-full ${color} opacity-100`}
-              />
-            )}
           </button>
         ))}
       </div>
